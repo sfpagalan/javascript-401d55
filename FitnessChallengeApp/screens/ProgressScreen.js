615bdf6de677223e04aead65f85bbf7d02183b5e
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-11-15 03:26:08 -0800

    added a camera feature

--- FitnessChallengeApp/screens/ProgressScreen.js
+++ FitnessChallengeApp/screens/ProgressScreen.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, Button, TouchableOpacity, Modal } from 'react-native';
 import ProgressBar from '../components/ProgressBar';
 import { extractTotalFromChallenge } from '../utility/challenges';
 import { ChallengesContext } from '../context/ChallengesContext';
+import * as FileSystem from 'expo-file-system';
 
 export default function ProgressScreen() {
-    const { completedChallenges } = useContext(ChallengesContext);
+    const { completedChallenges, savedImages, deleteImage } = useContext(ChallengesContext);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
 
-    // useEffect(() => {
-    //     if (route.params?.completedChallenges) {
-    //         setCompletedChallenges(route.params.completedChallenges);
-    //     }
-    // }, [route.params?.completedChallenges]);
+    const handleDeleteImage = async (imagePath) => {
+        try {
+          await FileSystem.deleteAsync(imagePath);
+          deleteImage(imagePath);
+        } catch (error) {
+          console.error("Error deleting image file:", error);
+        }
+    };
+
+    const openImageModal = (imagePath) => {
+        setSelectedImage(imagePath);
+        setModalVisible(true);
+    };
+
+    const closeImageModal = () => {
+        setModalVisible(false);
+        setSelectedImage(null);
+    };
 
     return (
         <ScrollView style={styles.container}>
@@ -30,6 +46,38 @@ export default function ProgressScreen() {
                     </View>
                 );
             })}
+            <Text style={styles.header}>Saved Images:</Text>
+            {savedImages.map((image, index) => (
+                <View key={index} style={styles.imageContainer}>
+                    <TouchableOpacity onPress={() => openImageModal(image)}>
+                        <Image source={{ uri: image }} style={styles.image} />
+                    </TouchableOpacity>
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={closeImageModal}>
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalView}>
+                                {selectedImage && <Image source={{ uri: selectedImage }} style={styles.enlargedImage} />}
+                                <TouchableOpacity 
+                                    style={styles.buttonContainer}
+                                    onPress={() => {
+                                        handleDeleteImage(selectedImage);
+                                        closeImageModal();
+                                    }}>
+                                    <Text style={styles.buttonText}>Delete Image</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity 
+                                    style={styles.closeButton}
+                                    onPress={closeImageModal}>
+                                    <Text style={styles.closeButtonText}>X</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </Modal>
+                </View>
+            ))}
         </ScrollView>
     );
 }
@@ -62,5 +110,68 @@ const styles = StyleSheet.create({
     },
     challengeText: {
         fontSize: 16,
+    },
+    imageContainer: {
+        alignItems: 'flex',
+        marginBottom: 15,
+    },
+    image: {
+        width: 200,
+        height: 200,
+        borderRadius: 10,
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 10,
+        right: 20,
+        backgroundColor: 'transparent',
+        padding: 8,
+        borderRadius: 15,
+    },
+    closeButtonText: {
+        fontSize: 30,
+        color: '#fff',
+        fontWeight: 'bold',
+        opacity: 0.8,
+    },
+    buttonContainer: {
+        backgroundColor: '#2A9D8F',
+        borderRadius: 5,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        marginVertical: 10,
+        alignItems: 'center',
+        marginTop: 15,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 15,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    enlargedImage: {
+        width: 350,
+        height: 600,
+        borderRadius: 10,
     },
 });
